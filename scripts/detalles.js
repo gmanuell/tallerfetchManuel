e3b0507833b14b3d58eb18f1b776c4a2ea4f5b2b
Manuel Garcia <manuelgarcia8295@gmail.com>
2024-08-25 08:58:01 -0500

    eliminar main

--- scripts/detalles.js
+++ scripts/detalles.js
@@ -1,5 +1,45 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const params = new URLSearchParams(window.location.search);
-//     const departmentId = params.get('id');
-//     const url = 'https://api-colombia.com/api/v1/department/${departmentId}';
+document.addEventListener("DOMContentLoaded", function() {
+    const params = new URLSearchParams(window.location.search);
+    const departmentId = params.get('id');
+    const url = 'https://api-colombia.com/api/v1/department/${departmentId}';
+})
 
+const url = window.location.search;
+const urlObjeto = new URLSearchParams(url);
+const detallesId = urlObjeto.get('id');
+
+if (detallesId) {
+    const event = data.events.find(e => e._id === detallesId);
+    
+     if (detalles) {
+        const cardContainer = document.getElementById("card-container"); 
+        const cardContent = document.createElement("div");
+        cardContent.className = "d-flex align-items-center ";
+
+        cardContent.innerHTML = `
+        <div class="cardDetails align-items-center col-12 border flex-column border-dark flex-sm-column d-flex flex-wrap flex-lg-row">
+            <div class="col-lg-6 p-0">
+                <img class="detailCard" src="${event.image}" alt="${event.name}">
+            </div>
+            <div class="flex-wrap col-6">
+                <div class="card-body">
+                    <h5 class="name d-card">${event.name}</h5>
+                    <p class="date d-card">${event.date}</p>
+                    <p class="description d-card">${event.description}</p>
+                    <p class="category d-card">${event.category}</p>
+                    <p class="place d-card">${event.place}</p>
+                    <p class="capacity d-card">Capacity: ${event.capacity}</p>
+                    ${attendanceOrEstimate}
+                    <p class="price d-card">Price: ${event.price} $</p>
+                </div>
+            </div>
+        </div>
+        `;
+
+        cardContainer.appendChild(cardContent);
+    } else {
+        console.log("Evento no encontrado");
+    }
+} else {
+    console.log("No se proporcionó ningún ID de evento en la URL");
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,17 @@
-import { cargarDepartamentos } from "./home.js";
-// import {cargarDetallesDepartamentos} from "./detalles.js";
-import { cargarEspeciesInvasoras } from "./especiesInvasoras.js";
+// import { cargarDepartamentos } from "./home.js";
+// // import {cargarDetallesDepartamentos} from "./detalles.js";
+// import { cargarEspeciesInvasoras } from "./especiesInvasoras.js";
 
 
-document.addEventListener("DOMContentLoaded", () => {
-    const path = window.location.pathname;
+// document.addEventListener("DOMContentLoaded", () => {
+//     const path = window.location.pathname;
    
-//     if (path.endsWith('home.html') || path === '/') {
-//         cargarDepartamentos();
-//     } else if (path.endsWith('detalles.html')) {
-//         cargarDetallesDepartamentos();
-//     } else if (path.endsWith('especies-invasoras.html')) {
-//         cargarEspeciesInvasoras();
-//     }
-if (path.endsWith('especies-invasoras.html') || path === '/') {
-             cargarEspeciesInvasoras();
-} else if (path.endsWith('home.html') || path === '/') {
-            cargarDepartamentos();
-}
-});
+// if (path.endsWith('especies-invasoras.html') || path === '/') {
+//              cargarEspeciesInvasoras();
+// } else if (path.endsWith('home.html') || path === '/') {
+//             cargarDepartamentos();
+// } 
+// // else if (path.endsWith('detalles.html')) {
+// //     cargarDetallesDepartamentos();
+// // }
+// });
